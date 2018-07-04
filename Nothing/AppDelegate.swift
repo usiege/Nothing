423211a0d23a63a6a356c90b59989e4e942973b9
lizhonghuan <2275572017@qq.com>
2018-07-04 19:11:17 +0800

    rename

--- Nothing/AppDelegate.swift
+++ Nothing/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        return true
-            
+//        return true
+        
         self.window = UIWindow.init(frame: UIScreen.main.bounds)
         self.window?.makeKeyAndVisible()
     
